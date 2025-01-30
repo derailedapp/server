a78d1cb7c08e37bd90ee9628a14f887e2f77676e
VincentRPS <vincentbusiness55@gmail.com>
2025-01-30 18:43:26 +0800

    chore: update license headers

--- ekranoplan/src/routes/tracks/mark_viewed.rs
+++ ekranoplan/src/routes/tracks/mark_viewed.rs
@@ -1,5 +1,5 @@
 /*
-   Copyright 2024 V.J. De Chico
+   Copyright 2024-2025 V.J. De Chico
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
